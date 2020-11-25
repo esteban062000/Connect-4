44887e9e0e623a2d3807b590898f7f3374498afd
ADRIAN.SIBAJARETANA <adrian.sibajaretana@ucr.acr.cr>
2020-11-25 13:20:36 -0600

    Se cambió el formato de identación del proyecto.
    - El formato cambia a ser 4 espacios
    - Los files se formatean cada vez que se guarda

--- Connect4.py
+++ Connect4.py
@@ -1,50 +1,40 @@
 from Connect4Utils import Connect4UtilsClass as Utils
-import Constants 
+import Constants
+
 
 class Connect4:
 
-   def __init__(self, player1, player2, gameIndex):
-      '''
-      Players: Random, Minimax, Human
-      '''
-      validPlayers = [Constants.PLAYER_MINIMAX, Constants.PLAYER_HUMAN, Constants.PLAYER_RANDOM]
-      if(player1 in validPlayers and player2 in validPlayers):
-         self.player1 = player1
-         self.player2 = player2 
-      else:
-         raise TypeError("Digite un jugador valido")
-      self.utils = Utils(gameIndex)
-      self.utils.createFile()
-      self.gameboard = self.utils.createGameBoard()
+    def __init__(self, player1, player2, gameIndex):
+        '''
+        Players: Random, Minimax, Human
+        '''
+        validPlayers = [Constants.PLAYER_MINIMAX,
+                        Constants.PLAYER_HUMAN, Constants.PLAYER_RANDOM]
+        if(player1 in validPlayers and player2 in validPlayers):
+            self.player1 = player1
+            self.player2 = player2
+        else:
+            raise TypeError("Digite un jugador valido")
+        self.utils = Utils(gameIndex)
+        self.utils.createFile()
+        self.gameboard = self.utils.createGameBoard()
 
-   def beginGame(self):
-      turnNumber = 1
-      activePlayer = ''
-      players = []
-      if (self.utils.computerGoesFirst()):
-         players.append(self.player1)
-         players.append(self.player2)
-      else:
-         players.append(self.player2)
-         players.append(self.player1)
-      while self.utils.getGameResult(self.gameboard) == Constants.GAME_STATE_NOT_ENDED:
-         activePlayer = players[turnNumber % 2]
-         turnNumber += 1
-   
-   def createGameBoard(self):
-        gameboard = [
-            [Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL],
-            [Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL],
-            [Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL],
-            [Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL],
-            [Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL],
-            [Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL]
-        ]
-        return gameboard
-    
-    # Returns a number in range [0, 1]
+    def beginGame(self):
+        turnNumber = 1
+        activePlayer = ''
+        players = []
+        if (self.computerGoesFirst()):
+            players.append(self.player1)
+            players.append(self.player2)
+        else:
+            players.append(self.player2)
+            players.append(self.player1)
+        while self.getGameResult(self.gameboard) == Constants.GAME_STATE_NOT_ENDED:
+            activePlayer = players[turnNumber % 2]
+            turnNumber += 1
+
     def computerGoesFirst(self):
-        return random.randint(0, 1)
+        return print("hello")
 
     def getAvailableMoves(self, gameboard):
         availableMoves = []
@@ -82,26 +72,27 @@ class Connect4:
         # Check lower left diagonals
         if not winnerFound:
             for i in range(Constants.NUM_ROWS - Constants.REQUIRED_SEQUENCE - 1):
-               j = 0
-               while j <= i:
-                   if gameboard[i][j] != 0 and gameboard[i][i] == gameboard[i + 1][j + 1] and gameboard[i][i] == gameboard[i + 2][j + 2] and \
-                           gameboard[i][i] == gameboard[i + 3][j + 3]:
-                       currentWinner = gameboard[i][j]
-                       winnerFound = True
-                   j = j+1
+                j = 0
+                while j <= i:
+                    if gameboard[i][j] != 0 and gameboard[i][i] == gameboard[i + 1][j + 1] and gameboard[i][i] == gameboard[i + 2][j + 2] and \
+                            gameboard[i][i] == gameboard[i + 3][j + 3]:
+                        currentWinner = gameboard[i][j]
+                        winnerFound = True
+                    j = j+1
 
         # Check upper right diagonals
         if not winnerFound:
             for j in range(Constants.NUM_COLUMNS - Constants.REQUIRED_SEQUENCE - 1):
                 i = j
-                while i<= Constants.NUM_ROWS - Constants.REQUIRED_SEQUENCE - 1:
+                while i <= Constants.NUM_ROWS - Constants.REQUIRED_SEQUENCE - 1:
                     if gameboard[i][j] != 0 and gameboard[i][i] == gameboard[i + 1][j + 1] and gameboard[i][i] == gameboard[i + 2][j + 2] and \
                             gameboard[i][i] == gameboard[i + 3][j + 3]:
                         currentWinner = gameboard[i][j]
                         winnerFound = True
                     i = i+1
 
-        if winnerFound: return currentWinner
+        if winnerFound:
+            return currentWinner
         else:
             drawFound = True
             # Check for draw
--- Connect4Utils.py
+++ Connect4Utils.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime
-import Constants 
+import Constants
 import random
 '''
 ***************************************************************************************/
@@ -12,16 +12,34 @@ import random
 ***************************************************************************************/
 '''
 
+
 class Connect4UtilsClass:
     def __init__(self, gameId):
         self.id = gameId
         now = datetime.now().strftime("%d-%m-%Y%H%M%S")
         self.fileName = f"./TrainingFiles/{now}{gameId}.txt"
-    
+
     def createFile(self):
         os.makedirs(os.path.dirname(self.fileName), exist_ok=True)
         f = open(self.fileName, "w")
-        f.close() 
+        f.close()
 
     def writeFile(self, currentGameState, moveDone):
         raise NotImplementedError
+
+    def createGameBoard(self):
+        gameboard = [
+            [Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL,
+                Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL],
+            [Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL,
+                Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL],
+            [Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL,
+                Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL],
+            [Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL,
+                Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL],
+            [Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL,
+                Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL],
+            [Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL,
+                Constants.EMPTY_VAL, Constants.EMPTY_VAL, Constants.EMPTY_VAL]
+        ]
+        return gameboard
